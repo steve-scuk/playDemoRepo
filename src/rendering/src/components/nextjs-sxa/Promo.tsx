4b51c83f65c02086ac6a51a7589951ee689c5c0b
Steve Little <steve.little@sitecore.com>
2022-12-22 16:11:23 +0000

    rebuild

--- src/rendering/src/components/nextjs-sxa/Promo.tsx
+++ src/rendering/src/components/nextjs-sxa/Promo.tsx
@@ -7,8 +7,7 @@ import {
   LinkField,
   RichTextField,
 } from '@sitecore-jss/sitecore-jss-nextjs';
-//
-//import * as FEAAS from '@sitecore-feaas/clientside/react';
+
 interface Fields {
   PromoIcon: ImageField;
   PromoIcon2: ImageField; // DEMO TEAM CUSTOMIZATION - Additional field
@@ -17,10 +16,12 @@ interface Fields {
   PromoText2: RichTextField; // DEMO TEAM CUSTOMIZATION - Change to RichTextField
   PromoText3: RichTextField; // DEMO TEAM CUSTOMIZATION - Additional field
 }
+
 type PromoProps = {
   params: { [key: string]: string };
   fields: Fields;
 };
+
 const PromoDefaultComponent = (props: PromoProps): JSX.Element => (
   <div className={`component promo ${props.params.styles}`}>
     <div className="component-content">
@@ -28,6 +29,7 @@ const PromoDefaultComponent = (props: PromoProps): JSX.Element => (
     </div>
   </div>
 );
+
 export const Default = (props: PromoProps): JSX.Element => {
   const id = props.params.RenderingIdentifier;
   if (props.fields) {
@@ -59,8 +61,10 @@ export const Default = (props: PromoProps): JSX.Element => {
       </div>
     );
   }
+
   return <PromoDefaultComponent {...props} />;
 };
+
 export const WithText = (props: PromoProps): JSX.Element => {
   const id = props.params.RenderingIdentifier;
   if (props.fields) {
@@ -86,8 +90,10 @@ export const WithText = (props: PromoProps): JSX.Element => {
       </div>
     );
   }
+
   return <PromoDefaultComponent {...props} />;
 };
+
 // DEMO TEAM CUSTOMIZATION - New variant
 export const WithColumns = (props: PromoProps): JSX.Element => {
   if (props.fields) {
@@ -120,44 +126,7 @@ export const WithColumns = (props: PromoProps): JSX.Element => {
       </div>
     );
   }
+
   return <PromoDefaultComponent {...props} />;
 };
 // END CUSTOMIZATION
-//adding in another comments line to force redeploy
-// Steve's Component  CUSTOMIZATION - New variant
-//eslint-disable-next-line @typescript-eslint/no-explicit-any
-// Component: AnotherPromo
-// Version:   Name
-// URL:       https://components.sitecorecloud.io/components/94akWripwj
-//eslint-disable-next-line @typescript-eslint/no-explicit-any
-//FFS just work
-/* 
-const MyComponent = ({data}: any) => (
-  <FEAAS.Component
-    library="2uDvpJVin0LumiN2UWcSI9"
-    component="94akWripwj"
-    version="2XVwwuQdkP"
-    revision="published"
-    hostname="https://feaascompontsapiprod.blob.core.windows.net"
-    data={data} />
-);
-const FromComponents = (props: PromoProps, version: string): JSX.Element => {
-  const { sitecoreContext } = useSitecoreContext();
-  console.log(version);
-  if (props.fields && !sitecoreContext.pageEditing) {
-    return (
-      <div className={`component ${props.params.styles} innerSpaceS`}>
-        <div className="component-content">
-        <FEAAS.Stylesheet
-  library="2uDvpJVin0LumiN2UWcSI9"
-  hostname="https://feaascompontsapiprod.blob.core.windows.net" />
-<MyComponent data={{"rXapTW7GUz":{"Text":"Promo Title Text","Image":"url_to_image_asset","Link":"https://lighthouse-hub.sitecoresandbox.cloud/api/public/content/f6fb36b2b2ce486fbaba68bdcb534792?v=8f2d362f","Text 2":"Promo Text Two","Text 3":"Promo Text Three"}}} />
-        </div>
-      </div>
-    );
-  } else if (props.fields && sitecoreContext.pageEditing) {
-    return Default(props);
-  }
-  return <PromoDefaultComponent {...props} />;
-};
-// END CUSTOMIZATION */
